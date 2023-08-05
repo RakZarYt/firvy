02ec3b4f8707268a7395f62bfa21f912bac58149
RakZarYt <68829371+RakZarYt@users.noreply.github.com>
2023-08-05 13:48:40 +0300

    глобальное обновление

---
+++ source/script/popuper.js
@@ -0,0 +1,57 @@
+// popuper
+// Находим попапы
+let popups = document.querySelectorAll('.popup'); // все попапы
+// открывающие элементы (*1)
+// let popupOpenEducation = document.querySelector('#popup-open-education'); 
+// let popupOpenVoice = document.querySelector('#popup-open-voice'); 
+let popupOpenVersion1 = document.querySelector('#popup-open-version1');
+let popupOpenVersion2 = document.querySelector('#popup-open-version2');
+let popupOpenVersion3 = document.querySelector('#popup-open-version3');
+
+
+// // открывающие элементы
+// popupOpenEducation.onclick = function (event) {
+//     let popupEducation = document.querySelector('#popup_education'); 
+//     if (popupEducation.style.display === 'block') {
+//         popupEducation.style.display = 'none';
+//     } else {
+//         popupEducation.style.display = 'block';
+//     }
+// }
+
+
+// popupOpenVoice.onclick = function (event) {
+//     let popupVoice = document.querySelector('#popup_voice'); 
+//     if (popupVoice.style.display === 'block') {
+//         popupVoice.style.display = 'none';
+//     } else {
+//         popupVoice.style.display = 'block';
+//     }
+// }
+
+popupOpenVersion1.onclick = function (event) {
+    let popupVersion1 = document.querySelector('#popup_version1'); 
+    if (popupVersion1.style.display === 'block') {
+        popupVersion1.style.display = 'none';
+    } else {
+        popupVersion1.style.display = 'block';
+    }
+}
+
+popupOpenVersion2.onclick = function (event) {
+    let popupVersion1 = document.querySelector('#popup_version2'); 
+    if (popupVersion1.style.display === 'block') {
+        popupVersion1.style.display = 'none';
+    } else {
+        popupVersion1.style.display = 'block';
+    }
+}
+
+popupOpenVersion3.onclick = function (event) {
+    let popupVersion1 = document.querySelector('#popup_version3'); 
+    if (popupVersion1.style.display === 'block') {
+        popupVersion1.style.display = 'none';
+    } else {
+        popupVersion1.style.display = 'block';
+    }
+}
